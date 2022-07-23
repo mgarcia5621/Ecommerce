b9cb7bae24562236d472844bd210793427b92dc7
Mateo <matthew27garcia@gmail.com>
2022-07-23 01:07:47 -0500

    deletions

--- scripts.js
+++ scripts.js
@@ -73,7 +73,6 @@ function showSlides(n) {
   let i = 1;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -84,6 +83,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
