451d4335fbcecc32f7b517b5f116c67bb3728a5c
Mateo <matthew27garcia@gmail.com>
2022-07-24 17:10:45 -0500

    added the changes for the clickable image

--- scripts.js
+++ scripts.js
@@ -85,3 +85,20 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("myImg");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.img;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
