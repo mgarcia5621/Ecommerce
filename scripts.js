40554e32b66db9acaf5d328e0d97485cac725fbc
Mateo <matthew27garcia@gmail.com>
2022-07-27 01:13:08 -0500

    added some css

--- scripts.js
+++ scripts.js
@@ -53,6 +53,13 @@ openNav.addEventListener('click', () => {
 })
 
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -67,12 +74,11 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-
-
 function showSlides(n) {
-  let i = 1;
+  let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -83,24 +89,8 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
